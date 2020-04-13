fd0387476c86c78fbe2d86cf97b58b7d5ed860b6
Nischay Dhiman <nischay.dhiman@weboniselab.com>
2020-04-13 16:44:22 +0530

    timestamps fix for server time

--- lib/zoom-ruby/sdk.rb
+++ lib/zoom-ruby/sdk.rb
@@ -19,7 +19,7 @@ module ZoomRuby
     end
 
     def get_signature(options = {})
-      time = (Time.now.to_i * 1000 - 30000).to_s
+      time = (Time.current.to_i * 1000 - 30000).to_s
       data = Base64.strict_encode64(self.class.default_params[:api_key] + options[:meeting_number] + time + options[:role].to_s)
       hash = Base64.strict_encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), self.class.default_params[:api_secret], data))
       tempStr = self.class.default_params[:api_key] + '.' + options[:meeting_number] + '.' + time + '.' + options[:role].to_s + '.' + hash
